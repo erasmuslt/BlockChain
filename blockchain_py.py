a2e076a46198b40f9e73a26613cebb7e3ad978e1
erasmuslt <vhrvnd66@gmail.com>
2018-09-06 15:12:58 +0900

    initial version

---
+++ blockchain_py.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 15:07:37 2018
+
+@author: KJH
+"""
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Block Chain
+
+class BlockChain:
+    
+
+
+# Part 2 - Mining Our Block
+
